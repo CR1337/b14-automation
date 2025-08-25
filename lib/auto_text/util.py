c32b9e7fdd69ceb17c9cbfc6dc7a2e6d90ed5ad3
Christian Raue <captain_2@gmx.de>
2025-08-25 11:33:25 +0200

    added language selection

--- lib/auto_text/util.py
+++ lib/auto_text/util.py
@@ -1,9 +1,18 @@
 from typing import List
 
 
-def enumerate_terms(terms: List[str], max_terms: int = -1) -> str:
+def enumerate_terms(terms: List[str], language: str = "de", max_terms: int = -1) -> str:
     if max_terms >= 0:
         terms = terms[:max_terms]
+    match language:
+        case "en":
+            and_ = "and"
+            as_well_as = "as well as"
+
+        case _:
+            and_ = "und"
+            as_well_as = "sowie"
+
     match len(terms):
         case 0:
             return ""
@@ -12,16 +21,16 @@ def enumerate_terms(terms: List[str], max_terms: int = -1) -> str:
             return terms[0]
 
         case 2:
-            return f"{terms[0]} und {terms[1]}"
+            return f"{terms[0]} {and_} {terms[1]}"
 
         case 3:
-            return f"{terms[0]} und {terms[1]} sowie {terms[2]}"
+            return f"{terms[0]} {and_} {terms[1]} {as_well_as} {terms[2]}"
 
         case 4:
-            return f"{terms[0]} und {terms[1]} sowie {terms[2]} und {terms[3]}"
+            return f"{terms[0]} {and_} {terms[1]} {as_well_as} {terms[2]} {and_} {terms[3]}"
 
         case _:
-            return f"{', '.join(t for t in terms[:-1])} und {terms[-1]}"
+            return f"{', '.join(t for t in terms[:-1])} {and_} {terms[-1]}"
 
 
 def format_value(value: float, decimal_places: int = 1) -> str:
